abfd66cb47a824580da09947db32d26539e0b1e6
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-11 21:38:21 +0800

    Update Seed.m

--- Seed.m
+++ Seed.m
@@ -1,2 +1,2 @@
 %Get "random" seed for generating random data (the following line is in R)
-set.seed(as.integer((as.double(Sys.time())*1000+Sys.getpid()) %% 2^31));
+set.seed(as.integer((as.double(Sys.time())*1000+Sys.getpid()) %% 2^31))
