76237d86c29ec4609e61883a627f191f6f9b2dfd
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-13 04:06:36 +0800

    Update MatrixAndVector.m

--- MatrixAndVector.m
+++ MatrixAndVector.m
@@ -1,5 +1,8 @@
 A(A>=0); %delete all negative elements
 
+%generate random data with nomalized distribution
+B = normrnd(μ,sd,rows,cols); % μ -> mean; sd -> standard deviation; rows -> number of rows; cols -> number of columns
+
 %Sort the rows of A based on the values in the second column. When the specified column has repeated elements, the corresponding rows maintain their original order.
 C = sortrows(A,2);
 D = sortrows(A,[1 7]); %Sort the rows of A based on the elements in the first column, and look to the seventh column to break any ties.
