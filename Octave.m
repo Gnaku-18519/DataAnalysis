1cdd61fa1e3d1ace8cfc0def93e3af73bf055420
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-14 10:26:58 +0800

    Create Octave.m

---
+++ Octave.m
@@ -0,0 +1,6 @@
+%{
+  To use the Debugging Mode, we need to:
+      1. Save the current script
+      2. Put the cursor to the break point line
+      3. Click the Break button to add a break point (and click again to disable this break point)
+%}
