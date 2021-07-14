5659cfde606aaabed5c352d1ae406f0ab59d3ae8
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-14 10:51:21 +0800

    Update Octave.m

--- Octave.m
+++ Octave.m
@@ -5,5 +5,6 @@
       3. Click the Break button to add a break point (and click again to disable this break point)
 %}
 
-pkg load statistics;
-%Might need to load packages before running (could be loaded at the beginning of a script or directly in the command window)
+pkg load statistics; %to load packages before running (could be loaded at the beginning of a script or directly in the command window)
+
+pkg install -forge XXX %used in the command window to implement packages from Octave Forge
