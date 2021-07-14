03e1df6285b7169f1a0508336c17de30aef3d769
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-14 10:29:54 +0800

    Update Octave.m

--- Octave.m
+++ Octave.m
@@ -4,3 +4,6 @@
       2. Put the cursor to the break point line
       3. Click the Break button to add a break point (and click again to disable this break point)
 %}
+
+pkg load statistics;
+%Might need to load packages before running (could be loaded at the beginning of a script or directly in the command window)
