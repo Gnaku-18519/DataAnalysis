45c09df3eb0e6f6c4cad6595414100aa6a530999
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-07-20 15:05:34 +0800

    Update Options.m

--- Options.m
+++ Options.m
@@ -18,15 +18,19 @@ Display (fminbnd, fminsearch, fzero, lsqnonneg):
 	'iter' displays intermediate steps (not available with lsqnonneg). See Optimization Solver Iterative Display.
 	'off' or 'none' displays no intermediate steps.
 	'final' displays just the final output.
+%}
 
+%{
 FunValCheck (fminbnd, fminsearch, fzero):
   Check whether objective function values are valid.
 	'on' displays an error when the objective function or constraints return a value that is complex or NaN.
 	'off' (default) displays no error.
-  
+%}
+
+options = optimset('MaxFunEvals', 50);
+%{
 MaxFunEvals (fminbnd, fminsearch):
   The maximum number of function evaluations allowed. The default value is 500 for fminbnd and 200*length(x0) for fminsearch.
-  eg. options = optimset('MaxFunEvals', 50);
 
 MaxIter	(fminbnd, fminsearch):
   The maximum number of iterations allowed. The default value is 500 for fminbnd and 200*length(x0) for fminsearch.
@@ -36,18 +40,22 @@ MaxIter	(fminbnd, fminsearch):
 	    you might tap the ground in front of you, in front and to the left, and in front and to the right to determine where the path is clear.
 	Once you know the direction where it's safe to move, you take a step and repeat the process of tapping and stepping.
 	Tapping the ground is a function evaluation. Stepping is an iteration. One step (iteration) may require multiple taps (function evaluations) to complete.
-  
+%}
+
+options = optimset('OutputFcn', @custom_stop_fun);
+function stop = custom_stop_fun(~, optimValues, ~)
+	if optimValues.fval < 20
+		stop = true;
+	else
+		stop = false;
+	end
+end
+%{
 OutputFcn (fminbnd, fminsearch, fzero):
   Display information on the iterations of the solver. The default is [] (none). See Optimization Solver Output Functions.
-  eg. options = optimset('OutputFcn', @custom_stop_fun);
-      function stop = custom_stop_fun(~, optimValues, ~)
-        if optimValues.fval < 20
-          stop = true;
-        else
-          stop = false;
-        end
-      end
-  
+%}
+
+%{
 PlotFcns (fminbnd, fminsearch, fzero):
   Plot information on the iterations of the solver. The default is [] (none). For available predefined functions, see Optimization Solver Plot Functions.
 
