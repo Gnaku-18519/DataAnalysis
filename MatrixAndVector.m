322d9cc2ecc30b241ecef5b2481e765d9c04e799
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-04 21:06:45 +0800

    Update MatrixAndVector.m

--- MatrixAndVector.m
+++ MatrixAndVector.m
@@ -8,3 +8,8 @@ C = sortrows(A,2);
 D = sortrows(A,[1 7]); %Sort the rows of A based on the elements in the first column, and look to the seventh column to break any ties.
 %Sort the rows of A in descending order based on the elements in the fourth column, and display the output vector index to see how the rows were rearranged.
 [E,index] = sortrows(A,4,'descend');
+
+row = size(matrix, 1); %count the number of rows for a matrix
+colNum = size(matrix, 2); %count the number of columns for a matrix
+
+n = nnz(matrix); %count the number of non-zero elements for a matrix
