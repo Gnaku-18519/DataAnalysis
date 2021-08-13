69771564fe5b795247af0410e5c76d48fed54533
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-13 16:49:39 +0800

    Update Function.m

--- Function.m
+++ Function.m
@@ -1,1 +1,22 @@
-%Derivative Formulas
+%Derivative
+%derivative of a variable
+syms x
+h = (2*x^2+1)/(3*x);
+diff(h)
+
+%derivative of a constant
+constant = sym('5');
+diff(constant)
+
+%second derivative
+f = cos(8*x);
+diff(f,2) %OR diff(diff(f))
+
+%partial derivative
+syms y z
+g = sin(y*z);
+diff(g,y)
+
+%derivative of a matrix
+A = [cos(4*x), 3*x; x, sin(5*x)];
+diff(A)
