ea6a00dd22a17d78f94101f21cb017b7ddec0e1f
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-17 04:07:56 +0800

    Update Package.m

--- Package.m
+++ Package.m
@@ -1,5 +1,5 @@
 %{
   After downloading a package from an external source (eg. mathwork.com), unpack the package.
   If we'd like to use the function in the package with an existing Matlab file, we should put all the unpacked package in the target file's directory.
-  If we simply want to use the function itself: type cd("XXX") in the command window to change directory to the directory of the function and the function is ready to be used.
+  If we simply want to use the function itself: type cd("XXX") in the command window to change directory to the directory of the function and the function is ready to be go.
 %}
