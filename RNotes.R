484a61542043dc857c0e3f0fcd375c8ff3c0e364
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-09-13 23:23:56 -0500

    Update RNotes.R

--- RNotes.R
+++ RNotes.R
@@ -1,2 +1,10 @@
 height <- 2 #assign value to a variable
 ls() #get all the existing variables
+
+class() #reveal type
+class(TRUE) #"logical"
+class(2) #"numerical"
+class(2L) %"integer"
+
+is.numeric(1) #TRUE
+as.numeric(TRUE) #1
